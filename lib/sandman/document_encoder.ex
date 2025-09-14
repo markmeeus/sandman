0e832cb28dd5515906617246d768df8dcb0c69a5
markmeeus <mark.meeus@gmail.com>
2025-09-14 16:50:23 +0200

    support read/write markdown blocks

--- lib/sandman/document_encoder.ex
+++ lib/sandman/document_encoder.ex
@@ -23,37 +23,69 @@ defmodule Sandman.DocumentEncoder do
   end
 
   defp write_block(encoded, block, true) do
-    encoded <> "```lua\n#{block.code}\n```\n"
+    encoded <> serialize_block(block)
   end
   defp write_block(encoded, block, false) do
-    encoded <> "\n```lua\n#{block.code}\n```\n"
+    encoded <> "\n" <> serialize_block(block)
   end
+
+  defp serialize_block(%{type: "lua", code: code}), do: "```lua\n#{code}\n```\n"
+  # markdown blocks are just markdown
+  defp serialize_block(block), do: block.code <> "\n"
 
 
   defp read_blocks(document, encoded, new_id_fn) do
-    {blocks, _current_block} = Enum.reduce(String.split(encoded, "\n"), {[], nil}, fn line, {blocks, current_block} ->
+    {blocks, last_block, _last_id} = Enum.reduce(String.split(encoded, "\n"), {[], nil, nil}, fn line, {blocks, current_block, last_id} ->
       case read_block_header(line) do
-        :lua -> case current_block do
-          # this is a header, is there no current block? then create one
-          nil -> {blocks, %{type: "lua", id: new_id_fn.(Enum.count(blocks)), code: "", state: :empty}}
-          current_block ->
-            # there is a current block, let's treat this a normal line
-            add_line_to_current_block({blocks, current_block}, line)
-        end
-
-        nil -> case current_block do # not a header line
-            nil -> {blocks, nil} # this wil later add to md current block
-            _block ->
+        :lua ->
+          case current_block do
+            # this is a lua header, if there no current block? then create one
+            nil ->
+              new_id = new_id_fn.(last_id)
+              {blocks, %{type: "lua", id: new_id, code: "", state: :empty}, new_id}
+            %{type: "lua"} ->
+              # there is a current lua block, let's treat this a normal line
+              # terminating lua blocks is handled by the add_line_to_current_block function
               add_line_to_current_block({blocks, current_block}, line)
+            _block ->
+              # no current lua block, lets add this one and create a new one
+              new_id = new_id_fn.(last_id)
+              {blocks ++ [current_block], %{type: "lua", id: new_id, code: "", state: :empty}, new_id}
+          end
+
+        # not a header line, this is either markdown or lua code
+        # depending on the type of the current block
+        # but that does not matter, we should just add the line
+        nil ->
+          case current_block do
+            nil ->
+              new_id = new_id_fn.(last_id)
+              {blocks, %{type: "markdown", id: new_id, code: line, state: :empty}, new_id}
+            _block ->
+              {blocks, current_block} = add_line_to_current_block({blocks, current_block}, line)
+              {blocks, current_block, last_id}
         end
       end
     end)
 
+    blocks = case last_block do
+      nil -> blocks
+      _block -> blocks ++ [last_block]
+    end
+    |> Enum.map(fn
+      block =%{type: "markdown", code: code} ->
+        block = if String.ends_with?(code, "\n") do
+          %{block | code: String.slice(code, 0..-2//1)}
+        else
+          block
+        end
+      block -> block
+    end)
+    |> Enum.filter(fn block -> block.code != "" end)
     Map.put(document, :blocks, blocks)
   end
 
   defp read_block_header(line) do
-    #block_header_regex = ~r/^<!-- sandman:(?<json>.*)-->$/
     block_header_regex = ~r/```(?<lang>.*)$/
     case (Regex.named_captures(block_header_regex, line)) do
       %{"lang" => "lua"} -> :lua
--- test/sandman/document_encoder_test.exs
+++ test/sandman/document_encoder_test.exs
@@ -3,6 +3,7 @@ defmodule Sandman.DocumentEncoderTest do
   use ExUnit.Case
   alias Sandman.DocumentEncoder
 
+  def new_id_fn(), do: &((&1||0) + 1)
 #   test "correctly encodes a document" do
 #     document = %{
 #       title: "My doc title",
@@ -35,7 +36,7 @@ defmodule Sandman.DocumentEncoderTest do
 
   test "reads empty document" do
     encoded = ""
-    assert DocumentEncoder.decode(encoded, &(&1)) == %{blocks: []}
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == %{blocks: []}
   end
 
 
@@ -50,8 +51,8 @@ ola pola
   end
   test "writes multiple blocks" do
     document = %{blocks: [
-      %{id: "867b6036-67a6-4afd-9857-050f21e24618", code: "ola pola"},
-      %{id: "867b6036-67a6-4afd-9857-050f21e24619", code: "ola\npola"}
+      %{id: "867b6036-67a6-4afd-9857-050f21e24618", code: "ola pola", type: "lua"},
+      %{id: "867b6036-67a6-4afd-9857-050f21e24619", code: "ola\npola", type: "lua"}
     ]}
     assert DocumentEncoder.encode(document) == """
 ```lua
@@ -65,12 +66,30 @@ pola
 """
   end
 
+  test "writes multiple blocks with markdown" do
+    document = %{blocks: [
+      %{id: "867b6036-67a6-4afd-9857-050f21e24618", code: "lua code block 1", type: "lua"},
+      %{id: "867b6036-67a6-4afd-9857-050f21e24620", code: "markdown", type: "markdown"},
+      %{id: "867b6036-67a6-4afd-9857-050f21e24619", code: "lua code block 2", type: "lua"},
+    ]}
+    assert DocumentEncoder.encode(document) == """
+```lua
+lua code block 1
+```
+
+markdown
+
+```lua
+lua code block 2
+```
+"""
+  end
+
   test "reads multiple blocks" do
     encoded = """
 ```lua
 ola pola1
 ```
-
 ```lua
 ola
 pola
@@ -82,9 +101,18 @@ pola
       %{id: 2, code: "ola\npola\n2", type: "lua", state: :empty}
     ]}
 
-    assert DocumentEncoder.decode(encoded, &(&1 + 1)) == document
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == document
   end
 
+
+  test "reads a single markdown block" do
+    encoded = "#title\nola pola"
+    document = %{blocks: [
+      %{id: 1, code: "#title\nola pola", type: "markdown", state: :empty},
+    ]}
+
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == document
+  end
 
   test "keeps newlines in text" do
     encoded = """
@@ -98,6 +126,51 @@ pola1
       %{id: 1, code: "ola\n\npola1", type: "lua", state: :empty},
     ]}
 
-    assert DocumentEncoder.decode(encoded, &(&1 + 1)) == document
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == document
+  end
+
+  test "reads combination of markdown and lua blocks" do
+    encoded = """
+#title
+ola pola
+
+```lua
+ola
+pola
+```
+"""
+    document = %{blocks: [
+      %{id: 1, code: "#title\nola pola", type: "markdown", state: :empty},
+      %{id: 2, code: "ola\npola", type: "lua", state: :empty},
+    ]}
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == document
+  end
+
+  test "reads combinations of markdown and lua blocks" do
+    encoded = """
+#title
+ola pola
+
+```lua
+ola
+pola
+```
+#title2
+ola pola
+2
+
+```lua
+ola
+pola
+2
+```
+"""
+    document = %{blocks: [
+      %{id: 1, code: "#title\nola pola", type: "markdown", state: :empty},
+      %{id: 2, code: "ola\npola", type: "lua", state: :empty},
+      %{id: 3, code: "#title2\nola pola\n2", type: "markdown", state: :empty},
+      %{id: 4, code: "ola\npola\n2", type: "lua", state: :empty},
+    ]}
+    assert DocumentEncoder.decode(encoded, new_id_fn()) == document
   end
 end
